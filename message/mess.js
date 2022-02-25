3236b4ff4e464a8aa4888ad606c85cb53d85acaf
armvxo <armvxo@gmail.com>
2022-02-26 06:33:57 +0700

    first commit

---
+++ message/mess.js
@@ -0,0 +1,18 @@
+module.exports.mess = {
+	        wait: 'Tunggu sebentar, lagi diproses!',
+			success: '[ √ ] 𝚂𝚞𝚌𝚌𝚎𝚜𝚜...~',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				stick: 'bukan sticker itu:v',
+				Iv: 'Link tidak valid',
+				api: 'Ups, terjadi kesalahan',
+				Ban: '*Maaf Tapi Kamu Sudah Terbanned Silahkan Minta Owner Untuk Membukanya*'
+			},
+			only: {
+				owner: ' ```-「 OWNER BOT ONLY 」-``` ',
+				admin: ' ```-「 ADMIN GROUP ONLY 」-``` ',
+				prem: '```-「 PREMIUM USER ONLY 」-```, kirim perintah *!buypremium* untuk membeli premium',
+				group: 'Maaf! Command ini khusus untuk di dalam Group saja.',
+				Badmin: 'Maaf! Command ini khusus untuk Bot ketika jadi admin!!',
+			}
+		}
